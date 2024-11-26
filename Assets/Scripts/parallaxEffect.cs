1fc9e48c8426d25c6902ca2d1d4842c1de072fba
Stephen McCaskie <McCaskie-S1@ulster.ac.uk>
2024-11-26 15:39:09 +0000

    movement and animations
    moves and flips

--- Assets/Scripts/parallaxEffect.cs
+++ Assets/Scripts/parallaxEffect.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class parallaxEffect : MonoBehaviour
 {
+    public Camera cam;
+    public Transform followTarget;
+
+    //Starting postion of the parallax game object
+    UnityEngine.Vector2 startingPosition;
+
+    //Start Z vaule of the parallax game object
+    float startingZ;
+    
+    //Distance that the camera has moved from the starting position of the parallax object
+    UnityEngine.Vector2 camMoveSinceStart => (UnityEngine.Vector2)cam.transform.position - startingPosition;
+
+    float zDistanceFromTarget => transform.position.z - followTarget.transform.position.z;
+
+    float clippingPlane => (cam.transform.position.z + (zDistanceFromTarget > 0 ? cam.farClipPlane : cam.nearClipPlane));
+
+    //The further the object is from the target the more it will move
+    float parallaxFactor => Mathf.Abs(zDistanceFromTarget) / clippingPlane;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.position;
+        startingZ = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UnityEngine.Vector2 newPosition = startingPosition + camMoveSinceStart * parallaxFactor;
+
+        transform.position = new UnityEngine.Vector3(newPosition.x, newPosition.y, startingZ);
     }
 }
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,13 +10,54 @@ public class playerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
     Vector2 moveInput;
-    public bool IsMoving { get; private set; }
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving { get
+        {
+            return _isMoving;
+        } 
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving?", value);
+        } 
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning?", value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight { get { return _isFacingRight;}  private set {
+        if(_isFacingRight != value)
+        {
+            transform.localScale *= new Vector2(-1, 1);
+        }
+
+        _isFacingRight = value;
+    }}
 
     Rigidbody2D rb;
+    Animator animator; 
 
-    void Awake()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
@@ -40,7 +83,38 @@ public class playerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
-        Debug.Log($"OnMove called. Move Input: {moveInput}, IsMoving: {IsMoving}");
+        //Debug.Log($"OnMove called. Move Input: {moveInput}, IsMoving: {IsMoving}");
+
+        SetFacingDirection(moveInput);
     }
 
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            //Face the right
+            IsFacingRight = true; 
+        }
+        else if(moveInput.x < 0 && IsFacingRight)
+        {
+            //Face the left
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        IsRunning = context.ReadValueAsButton();{}
+        {
+            if (context.started)
+            {
+                IsRunning = true;
+            } else if (context.canceled)
+            {
+                IsRunning = false;
+            }
+
+
+        }
+    }
 }
